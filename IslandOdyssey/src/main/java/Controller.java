fa665baf07c45c1378da27290d33c1c92d2dd3a5
Edwin <ebonillasorto@ggc.edu>
2023-04-20 22:02:24 -0400

    Moved useInteractable into Controller and added some functionality -Edwin

--- IslandOdyssey/src/main/java/Controller.java
+++ IslandOdyssey/src/main/java/Controller.java
@@ -211,6 +211,7 @@ public class Controller {
                             break;
                         } else {
                             view.printNoStrangeDevice();
+                            break;
                         }
                     //Paul
                     case "inventory":
@@ -261,8 +262,78 @@ public class Controller {
         }
     }
 
+    //Edwin
     private void useInteractable(Item tempItem) {
-        // move the interactable logic here
+        Interactable tempInteractable = (Interactable) tempItem;
+        if (tempInteractable.getHasUsedMaxAttempts() == true){
+            System.out.println("You can't use the phone again scrub!");
+            printMenu();
+        }
+        while (tempInteractable.getHasUsedMaxAttempts() == false){
+
+            if (tempInteractable.getAttempts() == 0){
+                tempInteractable.setHasUsedMaxAttempts(true);
+                view.printFailEnding();
+                printMenu();
+                break;
+            }
+            Scanner input = new Scanner(System.in);
+            System.out.println("An old satellite phone. You try dialing some numbers, but the call doesn’t go through." +
+                    " It is asking for the pin number. Enter 0000 to put the phone away");
+            String attempt = input.nextLine();
+
+            if (attempt.equalsIgnoreCase("0000")){
+                break;
+            }
+            else if (attempt.equals(tempInteractable.getPin())){
+                //Trigger ending
+                System.out.println("’Pin accepted.’\n" +
+                        "\n" +
+                        "Those words brighten up your entire life. The phone is waiting for you to dial the number of the party you wish to reach.\n" +
+                        "\n" +
+                        "You decide that calling 9-1-1 would be your best shot of reaching someone that could help you in this situation.\n" +
+                        "\n" +
+                        "It rings. It rings again. It rings again.\n" +
+                        "\n" +
+                        "‘9-1-1 what is your emergency?’\n" +
+                        "\n" +
+                        "Oh, thank heavens.\n" +
+                        "\n" +
+                        "‘H-hi, my name is [redacted], and I’ve been stranded on a deserted island with no knowledge of how I ended up here. The\n" +
+                        "\n" +
+                        "last thing I remember was working my shift at [redacted] and losing consciousness.’\n" +
+                        "\n" +
+                        "‘Okay, do you have any idea of where you are located right now?’\n" +
+                        "\n" +
+                        "‘I’m not entirely sure. This island looks deserted, but it’s got so much wildlife on it. There’s a mountain with a great view, a volcano, a lighthouse, a sky garden... It’s a bit of a shame nobody else is here, honestly. There’s also been a shipwreck on the edge of the island. I’m calling from a satellite phone I found in the sick bay.’\n" +
+                        "\n" +
+                        "The officer pauses for a moment. ‘...okay sir, we’re aware of a ship that’s been missing for a few weeks now. We’ll send helicopters out to look for an island with a ship and all these landmarks you’ve described. Please hang tight for as long as possible.’\n" +
+                        "\n" +
+                        "You detected a hint of distrust in the officer’s voice, but you’re not going to accuse the person who’s about to save your life of anything.\n" +
+                        "\n" +
+                        "‘Alright, thank you so much. I don’t know what I would’ve done without you.’\n" +
+                        "\n" +
+                        "What feels like a couple of hours has passed. You’ve made yourself as comfortable as possible on the ship while keeping tabs on the outside to check if any helicopters have flown by. You’ve also carved a large SOS in the sand just outside of the ship to hopefully indicate to anyone flying by of your location.\n" +
+                        "\n" +
+                        "A slight buzzing of helicopter rotors fills your ear. You rush out of the sickbay and out of the ship and scream at the top of your lungs. The helicopter pauses for a second in the air, then heads right for your location.\n" +
+                        "\n" +
+                        "The chopper lands near your location. You run up to the pilot and give them your most sincere thanks. They’re not too keen on conversating right now. You’re instructed to head into the back seat and answer questions that they have for you on the ride home. You are, of course, given some food and water before you set for the skies.\n" +
+                        "\n" +
+                        "It feels like an interrogation. What could you have possibly done wrong? You're asked various questions about names you’ve never heard in your life and what you were doing last month. You answer as honestly as possible, but you never shake the feeling of disco\n" +
+                        "\n" +
+                        "It’s been a year since the incident, and you feel more endangered at home than you felt on the island. The police suspect you’re the one responsible for the murder of the entire crew of the ship Mordor. It’s folklore at this point that you’re the one who caused the death of an entire crew. You’re not allowed to go 100 miles past the city limits. Your family understands your situation and knows the truth, but the long arm of the law may never know.\n" +
+                        "\n" +
+                        "The end.");
+                mainMenu();
+            }
+            else if (attempt.length() != 4 || !attempt.matches("[0-9]+")){
+                System.out.println("Pin must be 4 digits!");
+            }
+            else {
+                System.out.println("Wrong pin.");
+                tempInteractable.decrementAttempts();
+            }
+        }
     }
 
     //Bao
@@ -310,10 +381,9 @@ public class Controller {
                         case "f":
                         case "flip":
                             if(commands1.length != 2) {
-                                view.print("Please enter flip followed by switch number you would like to flip.");
+                                view.printFlipValidNumber();
                             } else if(!commands1[1].equals("1") && !commands1[1].equals("2") && !commands1[1].equals("3") && !commands1[1].equals("4") && !commands1[1].equals("5")) {
-                                view.print("Please enter flip and a valid number of the switch you would like to flip.");
-                                System.out.print(commands1[1]);
+                                view.printFlipValidNumber();
                             } else {
                                 ((Switches) room.getPuzzle()).flip(commands1[1]);
                                 view.print("You have flipped switch #" + commands1[1]);
